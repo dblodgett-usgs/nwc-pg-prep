e190dce309fed72b1a4c920c5e22b6c7fb1a3d41
David Blodgett <dblodgett@usgs.gov>
2016-03-03 09:30:13 -0600

    some case problems and correction to huc12agg projection handling

--- ogr2ogr_local.sh
+++ ogr2ogr_local.sh
@@ -34,9 +34,9 @@ psql -c "CREATE DATABASE \"nwcGeoserver\" WITH OWNER = \"$username\";" postgresq
 /usr/local/Cellar/gdal/1.11.1_3/bin/ogr2ogr -overwrite -progress -f "PostGreSQL" PG:"host=localhost user=$username password=$password dbname=nwcGeoserver" -nln "HUC12" -nlt PROMOTE_TO_MULTI -lco "GEOMETRY_NAME=the_geom" -lco "PRECISION=NO" -a_srs  HUC12_30June2015/data/WBDHU12_clip.prj  HUC12_30June2015/data/WBDHU12_clip.shp
 
 # NOTE: These data are not checked in and can be recreated with the R script included in the HUC12 directory. 
-/usr/local/Cellar/gdal/1.11.1_3/bin/ogr2ogr -overwrite -progress -f "PostGreSQL" PG:"host=localhost user=$username password=$password dbname=nwcGeoserver" -nln "HUC12Agg" -nlt PROMOTE_TO_MULTI -lco "GEOMETRY_NAME=the_geom" -lco "PRECISION=NO" -a_srs  EPSG:4269 -s_srs EPSG:4269 -t_srs EPSG:5070 HUC12_30June2015/HUC12_30June2015Agg/data/1.shp
+/usr/local/Cellar/gdal/1.11.1_3/bin/ogr2ogr -overwrite -progress -f "PostGreSQL" PG:"host=localhost user=$username password=$password dbname=nwcGeoserver" -nln "HUC12Agg" -nlt PROMOTE_TO_MULTI -lco "GEOMETRY_NAME=the_geom" -lco "PRECISION=NO" -a_srs  EPSG:4269 -s_srs EPSG:4269 HUC12_30June2015/HUC12_30June2015Agg/data/1.shp
 
-for i in {1..100}; do /usr/local/Cellar/gdal/1.11.1_3/bin/ogr2ogr -append -progress -f "PostGreSQL" PG:"host=localhost user=$username password=$password dbname=nwcGeoserver" -nln "HUC12Agg" -nlt PROMOTE_TO_MULTI -lco "GEOMETRY_NAME=the_geom" -lco "PRECISION=NO" -a_srs  EPSG:4269 -s_srs EPSG:4269 -t_srs EPSG:5070 HUC12_30June2015/HUC12_30June2015Agg/data/"$i"001.shp; done 
+for i in {1..100}; do /usr/local/Cellar/gdal/1.11.1_3/bin/ogr2ogr -append -progress -f "PostGreSQL" PG:"host=localhost user=$username password=$password dbname=nwcGeoserver" -nln "HUC12Agg" -nlt PROMOTE_TO_MULTI -lco "GEOMETRY_NAME=the_geom" -lco "PRECISION=NO" -a_srs  EPSG:4269 -s_srs EPSG:4269 HUC12_30June2015/HUC12_30June2015Agg/data/"$i"001.shp; done 
 
 /usr/local/Cellar/gdal/1.11.1_3/bin/ogr2ogr -overwrite -progress -f "PostGreSQL" PG:"host=localhost user=$username password=$password dbname=nwcGeoserver" -nln "gagesii_basins" -nlt PROMOTE_TO_MULTI -lco "GEOMETRY_NAME=the_geom" -lco "PRECISION=NO" -a_srs EPSG:5070 -t_srs EPSG:4326 gagesii_boundaries/bas_ref_all.shp
 
@@ -44,19 +44,19 @@ for i in gagesii_boundaries/nonref/*.shp; do /usr/local/Cellar/gdal/1.11.1_3/bin
 
 /usr/local/Cellar/gdal/1.11.1_3/bin/ogr2ogr -overwrite -progress -f "PostGreSQL" PG:"host=localhost user=$username password=$password dbname=nwcGeoserver" -nln "epa_basins" -nlt PROMOTE_TO_MULTI -lco "GEOMETRY_NAME=the_geom" -lco "PRECISION=NO" -a_srs EPSG:5070 -t_srs EPSG:4326 basins_all/basins_all.shp
 
-pg_dump -t huc12_SE_Basins_v2 postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/huc12_SE_Basins_v2.pgdump"
+pg_dump -t huc12_SE_Basins_v2 postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/huc12_se_basins_v2.pgdump"
 
-pg_dump -t huc12_SE_Basins_v2_local postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/huc12_SE_Basins_v2_local.pgdump"
+pg_dump -t huc12_SE_Basins_v2_local postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/huc12_se_basins_v2_local.pgdump"
 
-pg_dump -t US_Historical_Counties postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/US_Historical_Counties.pgdump"
+pg_dump -t US_Historical_Counties postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/us_historical_counties.pgdump"
 
-pg_dump -t HUC08 postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/HUC08.pgdump"
+pg_dump -t HUC08 postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/huc08.pgdump"
 
-pg_dump -t HUC12All postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/HUC12All.pgdump"
+pg_dump -t HUC12All postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/huc12all.pgdump"
 
-pg_dump -t HUC12 postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/HUC12.pgdump"
+pg_dump -t HUC12 postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/huc12.pgdump"
 
-pg_dump -t HUC12Agg postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/HUC12Agg.pgdump"
+pg_dump -t huc12agg postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/huc12agg.pgdump"
 
 pg_dump -t gagesii_basins postgresql://$username:$password@localhost/nwcGeoserver -O --file="dumps/gagesii_basins.pgdump"
 
