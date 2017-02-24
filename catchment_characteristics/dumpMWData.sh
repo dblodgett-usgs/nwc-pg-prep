b9d278278631d512977f9449b81bf49845a2fb28
David Blodgett <dblodgett@usgs.gov>
2017-02-24 07:51:01 -0600

    typo

--- catchment_characteristics/dumpMWData.sh
+++ catchment_characteristics/dumpMWData.sh
@@ -19,7 +19,7 @@
 #
 for file in *.pgdump; do gzip $file; done;
 
-# curl -u dblodgett --insecure -X PUT "https://cidasdpdasartip.cr.usgs.gov:8444/artifactory/nldi/datasets/characteristic_data.characteristic_data.characteristic_metadata.pgdump.gz" -T characteristic_data.characteristic_metadata.pgdump.gz -H "X-Checksum-Sha1:63620f28ec5a2d675882e9f953035f7875542131"
+# curl -u dblodgett --insecure -X PUT "https://cidasdpdasartip.cr.usgs.gov:8444/artifactory/nldi/datasets/characteristic_data.characteristic_metadata.pgdump.gz" -T characteristic_data.characteristic_metadata.pgdump.gz -H "X-Checksum-Sha1:63620f28ec5a2d675882e9f953035f7875542131"
 # curl -u dblodgett --insecure -X PUT "https://cidasdpdasartip.cr.usgs.gov:8444/artifactory/nldi/datasets/characteristic_data.divergence_routed_characteristics.pgdump.gz" -T characteristic_data.divergence_routed_characteristics.pgdump.gz -# -o log.txt -H "X-Checksum-Sha1:e4e4e2d3829dc3deda5dfc5e7918448cd915496a"
 # curl -u dblodgett --insecure -X PUT "https://cidasdpdasartip.cr.usgs.gov:8444/artifactory/nldi/datasets/characteristic_data.total_accumulated_characteristics.pgdump.gz" -T characteristic_data.total_accumulated_characteristics.pgdump.gz -# -o log.txt -H "X-Checksum-Sha1:3589b4c5a5fbd01a66840647f06bb24c10660f26"
 # curl -u dblodgett --insecure -X PUT "https://cidasdpdasartip.cr.usgs.gov:8444/artifactory/nldi/datasets/characteristic_data.local_catchment_characteristics.pgdump.gz" -T characteristic_data.local_catchment_characteristics.pgdump.gz -# -o log.txt -H "X-Checksum-Sha1:0965be8afc4f4bafb82af0b6ed4d16d56a01ed5c"
